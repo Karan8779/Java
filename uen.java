3e048a97efdda07212919023878a68cdfd08aa30
Karan8779 <71130973+Karan8779@users.noreply.github.com>
2020-09-17 10:38:28 +0530

    Add files via upload

---
+++ uen.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class uen {
+
+    public static void main(final String[] args) {
+        
+        int num1;
+ 
+            final Scanner in1 = new Scanner(System.in);
+            System.out.println("Enter the Number : ");
+            num1 = in1.nextInt();
+
+            in1.close();
+            System.out.println("The Number Entered : "+num1);
+    
+
+    }
+}    
